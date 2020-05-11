d1416c18af36b37e35d33424bb079aa3c49582aa
Donderstal <daanonderstal@hotmail.com>
2020-05-11 15:32:08 +0200

    vektis stuff for personal use

--- index.php
+++ index.php
@@ -3,8 +3,17 @@
 $request = $_SERVER['REQUEST_URI'];
 require __DIR__ . '/app/views/partials/head.phtml';
 
-require __DIR__ . '/app/views/partials/home.html';
-
-require __DIR__ . '/app/views/partials/foot.phtml';
-
+if ( strpos( $request, 'mapmaker' ) ) {
+    require __DIR__ . '/app/views/partials/head.phtml';
+    require __DIR__ . '/app/views/partials/mapmaker.html';    
+    require __DIR__ . '/app/views/partials/foot.phtml';
+}
+else if ( strpos( $request, 'vektis-viewer' ) ) {
+    require __DIR__ . '/app/views/partials/vektis-viewer.phtml';   
+}
+else {
+    require __DIR__ . '/app/views/partials/head.phtml';
+    require __DIR__ . '/app/views/partials/home.html';    
+    require __DIR__ . '/app/views/partials/foot.phtml';
+}
 ?>
