ea3d1b6559fcacc66a15e8e2bfb6850b0197582c
Donderstal <daanonderstal@hotmail.com>
2020-09-16 15:21:44 +0200

    Adding new sheet selection modal

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 $request = $_SERVER['REQUEST_URI'];
 require __DIR__ . '/app/views/head.phtml';
 require __DIR__ . '/app/views/mapmaker.html';    
+require __DIR__ . '/app/views/tilesheetmodal.html';    
 require __DIR__ . '/app/views/foot.phtml';
 
 ?>
