b65d6e06fff4747a99c6f4876925fe3a880cff7b
Donderstal <daanonderstal@hotmail.com>
2020-09-16 14:09:18 +0200

    restructuring files

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 /* Catch requested route */
 $request = $_SERVER['REQUEST_URI'];
-require __DIR__ . '/app/views/partials/head.phtml';
-require __DIR__ . '/app/views/partials/mapmaker.html';    
-require __DIR__ . '/app/views/partials/foot.phtml';
+require __DIR__ . '/app/views/head.phtml';
+require __DIR__ . '/app/views/mapmaker.html';    
+require __DIR__ . '/app/views/foot.phtml';
 
 ?>
