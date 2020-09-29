d0f26e084fbc6ffe3ce89fd98abf90ee5506b0c0
Donderstal <daanonderstal@hotmail.com>
2020-09-29 14:43:26 +0200

    implementing new class-based architecture

--- public/js/canvasScript.js
+++ public/js/canvasScript.js
@@ -4,9 +4,9 @@ const initializeCanvas = ( name, width, height, image ) => {
 
     canvas.width = width;
     canvas.height = height;
-    ctx.drawImage( image, 0, 0, image.width, image.height, 0, 0, width, height )
+    ctx.drawImage( image, 0, 0, image.width, image.height, 0, 0, width, height );
 
-    canvas.image = image
+    canvas.image = image;
 }
 
 const setTilesheet = ( src ) => {
@@ -20,35 +20,23 @@ const setTilesheet = ( src ) => {
         initializeCanvas( "SHEET", sheetWidthInApp, sheetHeightInApp, image );
         initializeCanvas( "HIDDEN", sheetWidthInApp, sheetHeightInApp, image );
 
-        drawGrid( "SHEET", sheetHeightInApp / TILE_SIZE, 4 )
+        SHEET.initGrid( sheetHeightInApp / TILE_SIZE, 4 );
     }
 }
 
-const initMapCanvas = ( cells ) => {
-    MAP_CANVAS.width = cells.cols * TILE_SIZE
-    MAP_CANVAS.height = cells.rows * TILE_SIZE
+const initMapCanvas = ( cell ) => {
+    MAP_CANVAS.width = cell.cols * TILE_SIZE
+    MAP_CANVAS.height = cell.rows * TILE_SIZE
 
-    drawGrid( "MAP", cells.rows, cells.cols )
-}
-
-const drawGrid = ( canvas, rows, cols ) => {
-    const ctx = canvas == "MAP" ? MAP_CTX : SHEET_CTX;
-
-    for ( var i = 0; i <= ( rows - 1 ); i++ ) {
-        x = 0
-        y = i * TILE_SIZE
-        for ( var j = 0; j <= ( cols - 1 ) ; j++ ) {
-            ctx.beginPath();
-            ctx.moveTo( x, y );
-            ctx.lineTo( x, y + TILE_SIZE )
-            ctx.moveTo( x, y );
-            ctx.lineTo( x + TILE_SIZE, y )
-            ctx.stroke()
-            x += TILE_SIZE 
-        }
-    }
+    MAP.initGrid( cell.rows, cell.cols )
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    initMapCanvas({ rows: MAX_ROWS, cols: MAX_COLS })
+    const sheetX = SHEET_CANVAS.getBoundingClientRect( ).x;
+    const sheetY = SHEET_CANVAS.getBoundingClientRect( ).y;
+    const mapX = MAP_CANVAS.getBoundingClientRect( ).x;
+    const mapY = MAP_CANVAS.getBoundingClientRect( ).y;
+
+    SHEET = new Sheet( sheetX, sheetY );
+    MAP = new Map( mapX, mapY );
 });
