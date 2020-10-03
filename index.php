d7b756303dfb93bc4a97e76adb58e3fd33536388
Donderstal <daanonderstal@hotmail.com>
2020-10-03 14:06:30 +0200

    experiment with html partials

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
-/* Catch requested route */
-$request = $_SERVER['REQUEST_URI'];
-require __DIR__ . '/app/views/head.phtml';
-require __DIR__ . '/app/views/mapmaker.html';    
-require __DIR__ . '/app/views/tilesheetmodal.html';    
-require __DIR__ . '/app/views/foot.phtml';
-
+    /* Catch requested route */
+    $request = $_SERVER['REQUEST_URI'];
+    require __DIR__ . '/app/views/head.phtml';
+    require __DIR__ . '/app/views/mapmaker.html';    
+    require __DIR__ . '/app/views/login.html';
+    require __DIR__ . '/app/views/welcome.html';
+    require __DIR__ . '/app/views/tilesheetmodal.html';    
+    require __DIR__ . '/app/views/foot.phtml';
 ?>
