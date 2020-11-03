8e80058aecf8f04aad0bc66ac3a6dd19359e274c
Donderstal <daanonderstal@hotmail.com>
2020-11-03 16:48:10 +0100

    drawing tiles no longer copies grid borders

--- public/js/structures/canvasWithGrid.js
+++ public/js/structures/canvasWithGrid.js
@@ -87,7 +87,7 @@ class Map extends CanvasWithGrid {
     getTileAtXY( x, y ) {
         const tile = super.getTileAtXY( x, y );
         tile.setTileID( SHEET.activeTile.index )
-        MAP_CTX.drawImage( SHEET_CANVAS, SHEET.activeTile.x, SHEET.activeTile.y, TILE_SIZE, TILE_SIZE, tile.x, tile.y, TILE_SIZE, TILE_SIZE )
+        MAP_CTX.drawImage( SHEET_CANVAS.image, SHEET.activeTile.x * 2, SHEET.activeTile.y * 2, TILE_SIZE * 2, TILE_SIZE * 2, tile.x, tile.y, TILE_SIZE, TILE_SIZE )
     }
 
     exportMapData( ) {
