3b14e7850eeda86a92dc81a37c002f97edeb3a06
Donderstal <daanonderstal@hotmail.com>
2021-11-01 21:33:57 +0100

    implementing css grid

--- public/js/mapOverview.js
+++ public/js/mapOverview.js
@@ -1,3 +1,4 @@
+/// Scrolling listeners;
 const stopMapOverviewScroll = ( ) => {
     IS_OVERVIEW_SCROLL_ACTIVE = false;
 }
@@ -22,60 +23,6 @@ const clearOverviewWrapperElements = ( ) => {
     removeAllChildrenFromParent( "map-overview-canvas-wrapper" );
 }
 
-const setLoadMapListeners = ( ) => {
-    Array.from(document.getElementsByClassName('load-from-overview')).forEach( ( el ) => {
-        el.addEventListener( 'click', ( e ) => {
-            const mapName = e.target.id.split('-')[2];
-            const activeMaps = IN_SUBMAP_OVERVIEW ? MAP_OVERVIEW_CURRENT_SUBMAP : MAP_OVERVIEW_CURRENT_NEIGHBOURHOOD
-            const keysList = Object.keys( activeMaps )
-            const map = activeMaps[keysList[mapName]]
-
-            TILESHEET_TO_LOAD = map["tileSet"];
-            NEIGHBOURHOOD_TO_LOAD = map["neighbourhood"]
-            MAPNAME_TO_LOAD = map["mapName"]
-            COLUMNS_TO_LOAD = map["columns"];
-            ROWS_TO_LOAD = map["rows"];
-            GRID_TO_LOAD = map["grid"].flat(1);
-            CHARACTERS_TO_LOAD = map["characters"];
-            OBJECTS_TO_LOAD = map["mapObjects"];
-
-            loadMapToMapmaker( );
-            document.getElementsByClassName('window-active')[0].className = "row window window-inactive";
-            document.getElementById("mapmaker-div").className = "window-active";
-        } )
-    })
-}
-
-const getCanvasElementsListFromMapJSON = ( ) => {
-    let canvasElementsList = [ ];
-    let overviewClassList = "overview-canvas";
-    let Xcounter = 0;
-
-    const json = IN_SUBMAP_OVERVIEW ? MAP_OVERVIEW_CURRENT_SUBMAP : MAP_OVERVIEW_CURRENT_NEIGHBOURHOOD
-
-    Object.keys( json ).forEach( ( mapName, index ) => {
-        canvasElementsList.push( { 
-            mapCanvas: createNodeWithClassOrID( 'canvas', overviewClassList, mapName ),
-            infoCanvas: createNodeWithClassOrID( 'canvas', overviewClassList ),
-            buttonsDiv: createNodeWithClassOrID( 'div', overviewClassList ),
-            mapData: json[mapName],
-            mapClass : null,
-            index: index
-        } );
-
-        Xcounter += json[mapName].columns * TILE_SIZE;
-    } );
-
-    setMapOverviewElementsTotalWidth( Xcounter )
-
-    return canvasElementsList
-}
-
-const setOverviewCanvas = ( mapCanvas ) => {
-    mapCanvas.width = MAX_CANVAS_WIDTH;
-    mapCanvas.height = MAX_CANVAS_HEIGHT;    
-    OVERVIEW_CANVAS_WRAPPER.append( mapCanvas )
-}
 
 const setMapClass = ( mapClass, mapCanvas, mapData, canvasX, canvasY ) => {
     mapClass      = new Map( canvasX, canvasY, mapCanvas.getContext( "2d" ) );
@@ -107,47 +54,42 @@ const handleShowSubMapsButtonClick = ( event ) => {
 }
 
 const initializeMapOverviewCanvases = ( ) => {
-    let canvasX = 0;
-    let canvasY = OVERVIEW_CANVAS_WRAPPER.getBoundingClientRect( ).y
-
-    let canvasElementsList = getCanvasElementsListFromMapJSON( );
-    canvasElementsList.forEach( ( e ) => {
-        setCanvasElementData( e, canvasX, canvasY );
-        canvasX += e.mapData.columns * TILE_SIZE;
-    } );
-
-    Array.from(document.getElementsByClassName('show-submaps')).forEach( ( button ) => {
-        button.addEventListener( 'click', handleShowSubMapsButtonClick, true )
-    } )
-
-    setLoadMapListeners( );
+    console.log(MAP_OVERVIEW_CURRENT_NEIGHBOURHOOD);
+    new CanvasGrid(MAP_OVERVIEW_CURRENT_NEIGHBOURHOOD)
 }
 
 class CanvasSlot {
-    constructor( key ) {
+    constructor( key, column, row ) {
         this.isSet = false;
         this.key = key;
+        this.column = column;
+        this.row = row;
+        this.setCanvas( );
+    }
+
+    setCanvas( ) {
         this.canvas = createNodeWithClassOrID( 'canvas', "overview-canvas", this.key );
+        this.canvas.width = MAX_CANVAS_WIDTH;
+        this.canvas.height = MAX_CANVAS_HEIGHT;
+        this.canvas.style.gridRow = this.row;
+        this.canvas.style.gridColumn = this.column;
+        OVERVIEW_CANVAS_WRAPPER.append( this.canvas );        
     }
 
     loadMapToCanvas( data ) {
-        
+        console.log(data)
     }
 }
 
 class CanvasGrid {
     constructor( data ){
-        data.horizontal_slots.forEach((slotChar, index) => { 
+        [...data.horizontal_slots].forEach((slotChar, index) => { 
             let horiChar = slotChar;
             let horiIndex = index;
-            data.vertical_slots.forEach((e, vertIndex ) => {
-                this[horiChar+e] = new CanvasSlot(horiChar+e);
+            [...data.vertical_slots].forEach((e, vertIndex) => {
+                this[horiChar+e] = new CanvasSlot(horiChar+e, horiIndex + 1, vertIndex + 1);
+                this[horiChar+e].loadMapToCanvas(data[horiChar+e]);
             });
-        })
-        Object.keys(data.grid).forEach((key) => { 
-
         })
     }
 }
-
-new CanvasGrid( ["A", "B", "C", "D", "E"], ["1", "2", "3", "4"] )
